6330b1c4a97bcc8a49d1b5a89dab083dab0e97d2
Laridn <larissa.nasci@hotmail.com>
2023-03-02 19:57:38 -0300

    Retirado only do teste

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -22,7 +22,7 @@ context('Funcionalidade Login', () => {
       'Endereço de e-mail desconhecido'
     );
   });
-  it.only('Deve exibir mensagem de erro ao inserir senha inválida', () => {
+  it('Deve exibir mensagem de erro ao inserir senha inválida', () => {
     cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/');
     cy.get('#username').type('aluno@teste.com');
     cy.get('#password').type('teste@teste');
