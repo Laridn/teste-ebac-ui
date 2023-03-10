607e09bb7338ec0e3970d357e7a0e291fd752dfd
Laridn <larissa.nasci@hotmail.com>
2023-03-09 23:08:49 -0300

    Criado testes endereço faturamento

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/pages-objects/endereco.page';
+const dadosEndereco = require('../fixtures/endereco.json');
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
   beforeEach(() => {
@@ -8,6 +10,41 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     });
   });
   it('Deve fazer cadastro de funcionamento com sucesso', () => {
-    //cadastro
+    EnderecoPage.editarEnderecoFaturamento(
+      'Larissa',
+      'Nascimento',
+      'Nubank',
+      'Brasil',
+      'Rua da Paz',
+      '1001',
+      'São Paulo',
+      'São Paulo',
+      '03974040',
+      '11999999999',
+      'teste@teste.com'
+    );
+    cy.get('.woocommerce-message').should(
+      'contain',
+      'Endereço alterado com sucesso.'
+    );
+  });
+  it('Deve fazer cadastro de funcionamento com sucesso - Usando arquivos de dados', () => {
+    EnderecoPage.editarEnderecoFaturamento(
+      dadosEndereco[2].nome,
+      dadosEndereco[2].sobrenome,
+      dadosEndereco[2].empresa,
+      dadosEndereco[2].pais,
+      dadosEndereco[2].endereco,
+      dadosEndereco[2].numero,
+      dadosEndereco[2].cidade,
+      dadosEndereco[2].estado,
+      dadosEndereco[2].cep,
+      dadosEndereco[2].telefone,
+      dadosEndereco[2].email
+    );
+    cy.get('.woocommerce-message').should(
+      'contain',
+      'Endereço alterado com sucesso.'
+    );
   });
 });
